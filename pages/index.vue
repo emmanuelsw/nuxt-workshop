700cb999b203c7e387897d4bee7760e624e0bc65
EmmanuelSW <edhgt89@hotmail.com>
2018-10-23 22:24:11 -0500

    Nuxt initial commit & fetch data

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Albums de la página</h1>
+    <div v-for="album in albums" :key="album.id">
+      <h5>{{album.title}}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import env from '../config/env'
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      albums: []
+    }
+  },
+  created() {
+    axios.get(`${env.endpoint}/albums`)
+    .then(response => {
+      this.albums = response.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
